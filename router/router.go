eae624773ef0c56a3488576de3be313d1d62d48b
jkim7113 <jkim7113@gmail.com>
2023-12-06 02:32:42 -0600

    Frontend Setup

--- router/router.go
+++ router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 	"os"
 
@@ -15,13 +16,16 @@ func NewRouter(articleController *controller.ArticleController) *chi.Mux {
 	fmt.Printf("Server Listening on PORT %s \n", portString)
 
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		msg := fmt.Sprintf("Your current IP address is %s", r.RemoteAddr)
-		w.WriteHeader(200)
-		w.Write([]byte(msg))
+		// msg := fmt.Sprintf("Your current IP address is %s", r.RemoteAddr)
+		// w.Write([]byte(msg))
+		// w.WriteHeader(200)
+		tmpl := template.Must(template.ParseFiles("./views/index.html"))
+		tmpl.Execute(w, nil)
 	})
 
 	router.Get("/articles/{Id}", articleController.FindById)
 	router.Post("/articles", articleController.Create)
+	router.Handle("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("./public"))))
 
 	return router
 }
